48130054319724ee0d70fc7e2bd8062ced4896b4
reshviatGH020 <reshvi.wbds@gmail.com>
2025-06-16 13:04:40 +0530

    Delete borderfade.js

--- borderfade.js
+++
@@ -1,8 +0,0 @@
-  const toggleBtn = document.getElementById('menuToggle');
-  const menu = document.querySelector('.menu');
-
-  toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('open');
-    toggleBtn.classList.toggle('no-border');
-  }
-);
